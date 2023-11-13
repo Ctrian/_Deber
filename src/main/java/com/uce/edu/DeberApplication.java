2aebb089aa057ef10a289a826e9e31d6055e7e9d
Cristian Olivares <andrescalvache47@gmail.com>
2023-11-13 00:31:43 -0500

    Cambio dentro de la misma rama

--- src/main/java/com/uce/edu/DeberApplication.java
+++ src/main/java/com/uce/edu/DeberApplication.java
@@ -15,6 +15,7 @@ public class DeberApplication implements CommandLineRunner{
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
 		System.out.println("Cristian Olivares - Taller 5");
+		System.out.println("Cambio en la misma rama");
 	}
 
 }
